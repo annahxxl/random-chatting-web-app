0fecf284d3592160c5d87c7438d8eaef0a15e1e4
Hanna Lee <8annahxxl@gmail.com>
2021-08-25 23:17:39 +0900

    마무리

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,12 +2,12 @@ const socket = io('/chattings');
 
 const getElementById = (id) => document.getElementById(id) || null;
 
-// get DOM element
+//* get DOM element
 const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
-// global socket handler
+//* global socket handler
 socket.on('user_connected', (username) => {
   drawNewChat(`${username} connected!`);
 });
@@ -17,25 +17,39 @@ socket.on('new_chat', (data) => {
 });
 socket.on('disconnect_user', (username) => drawNewChat(`${username}: bye...`));
 
-// draw functions
-const drawHelloStranger = (username) =>
-  (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
-const drawNewChat = (message) => {
-  const wrapperChatBox = document.createElement('div');
-  const chatBox = `<div>${message}</div>`;
-  wrapperChatBox.innerHTML = chatBox;
-  chattingBoxElement.append(wrapperChatBox);
-};
-
-// event callback functions
+//* event callback functions
 const handleSubmit = (event) => {
   event.preventDefault();
   const inputValue = event.target.elements[0].value;
   if (inputValue !== '') {
     socket.emit('submit_chat', inputValue);
-    drawNewChat(`me: ${inputValue}`);
+    // 화면에다가 그리기
+    drawNewChat(`me : ${inputValue}`, true);
     event.target.elements[0].value = '';
   }
+};
+
+//* draw functions
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+const drawNewChat = (message, isMe = false) => {
+  const wrapperChatBox = document.createElement('div');
+  wrapperChatBox.className = 'clearfix';
+  let chatBox;
+  if (!isMe)
+    chatBox = `
+    <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  else
+    chatBox = `
+    <div class='bg-white w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
 };
 
 function helloUser() {
@@ -47,7 +61,6 @@ function helloUser() {
 
 function init() {
   helloUser();
-
   // 이벤트 연결
   formElement.addEventListener('submit', handleSubmit);
 }
